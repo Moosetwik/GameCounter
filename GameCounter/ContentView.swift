af7e446bb6c78f60a6991fac8babf8404968c929
Nick Jones <nickjones@live.com.au>
2022-04-09 23:32:41 +1000

    Added separate player screens 	-Button functionality 		-+/- based on side of screen tapped

--- GameCounter/ContentView.swift
+++ GameCounter/ContentView.swift
@@ -9,10 +9,91 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        
+        
+        
+        VStack(spacing: 0) {
+            
+            //Upper Counter
+            
+            PlayerRectView(rectColor: .purple)
+                .rotationEffect(.degrees(180))
+            
+            
+            Rectangle()
+                .foregroundColor(.black)
+                .frame(height: 50)
+                
+                
+            PlayerRectView(rectColor: .blue)
+            
+            
+        }
+        .ignoresSafeArea()
+        
     }
 }
+
+struct PlayerRectView: View {
+    
+    @State private var lifeTotal = 20
+    
+    var rectColor: Color
+    
+    var body: some View {
+        
+        ZStack{
+            Rectangle()
+                .foregroundColor(rectColor)
+            Text("\(lifeTotal)")
+               
+                
+                .frame(maxWidth: .infinity, maxHeight: .infinity)
+                .font(.system(size: 220))
+                .minimumScaleFactor(0.1)
+                .lineLimit(1)
+            
+            HStack{
+               
+                LifeChangeButton(lifeTotal: $lifeTotal, isPositive: false)
+              LifeChangeButton(lifeTotal: $lifeTotal, isPositive: true)
+                
+            }
+               
+                
+            
+        }
+        
+    }
+}
+
+struct LifeChangeButton: View {
+    
+    @Binding var lifeTotal: Int
+    let isPositive: Bool
+    
+    var body: some View {
+        Button {
+            
+            if isPositive {
+                lifeTotal += 1
+                print(isPositive)
+            } else {
+                lifeTotal -= 1
+            }
+            
+            
+        } label: {
+            Text("")
+                .frame(maxWidth: .infinity, maxHeight: .infinity)
+        }
+    }
+}
+
+struct CentreView: View {
+    
+}
+
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
