a6063d71d6d5b13f4ee92e2062aa47e8fcc6cddb
Nick Jones <nickjones@live.com.au>
2022-04-09 22:16:36 +1000

    Initial Commit

---
+++ GameCounter/GameCounterApp.swift
@@ -0,0 +1,17 @@
+//
+//  GameCounterApp.swift
+//  GameCounter
+//
+//  Created by Nick Jones on 9/4/2022.
+//
+
+import SwiftUI
+
+@main
+struct GameCounterApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
