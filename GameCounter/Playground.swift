25df159148dffa005c103ae7d75f35082e1a8ec3
Nick Jones <nickjones@live.com.au>
2022-05-29 19:46:24 +1000

    .

---
+++ GameCounter/Playground.swift
@@ -0,0 +1,23 @@
+//
+//  Playground.swift
+//  GameCounter
+//
+//  Created by Nick Jones on 29/5/2022.
+//
+
+import SwiftUI
+
+struct Playground: View {
+    var body: some View {
+        ZStack{
+            Text("Hey")
+        }
+    }
+}
+
+struct Playground_Previews: PreviewProvider {
+    static var previews: some View {
+        Playground().previewLayout(.sizeThatFits)
+            .padding(20)
+    }
+}
